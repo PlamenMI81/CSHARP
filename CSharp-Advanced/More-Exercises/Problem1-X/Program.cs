85ace9cce9a17e7c328fb7100bf72e148c7dc356
PlamenMI81-D <plamko@abv.bg>
2017-08-25 21:55:15 +0300

    new excec

---
+++ CSharp-Advanced/More-Exercises/Problem1-X/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem1_X
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int middleSpace = n - 2;
+            int leftSpace = 0;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                leftSpace = i;
+                Console.WriteLine("{0}x{1}x", new string(' ', leftSpace), new string(' ', middleSpace));
+                middleSpace -= 2;
+            }
+            leftSpace++;
+            Console.WriteLine("{0}x", new string(' ', leftSpace));
+            leftSpace--;
+            middleSpace = 1;
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine("{0}x{1}x", new string(' ', leftSpace), new string(' ', middleSpace));
+                leftSpace--;
+                middleSpace += 2;
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/More-Exercises/Problem2-Vapor-Store/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Problem2_Vapor_Store
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            decimal moneyBalance = decimal.Parse(Console.ReadLine());
+            string game = string.Empty;
+            decimal price = 0.0m;
+            decimal sumSpent = 0.0m;
+
+            for (int i = 1;; i++)
+            {
+                game = Console.ReadLine();
+                switch (game)
+                {
+                    case "OutFall 4":
+                        price = 39.99m;
+                        break;
+                    case "CS: OG":
+                        price = 15.99m;
+                        break;
+                    case "Zplinter Zell":
+                        price = 19.99m;
+                        break;
+                    case "Honored 2":
+                        price = 59.99m;
+                        break;
+                    case "RoverWatch":
+                        price = 29.99m;
+                        break;
+                    case "RoverWatch Origins Edition":
+                        price = 39.99m;
+                        break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
