2b884feb55248b2e8b6d8be4e0c391466f80ac5f
PlamenMI81-D <plamko@abv.bg>
2017-09-16 17:18:33 +0300

    added problem2

--- CSharp-Advanced/More-Exercises/Problem2-Vapor-Store/Program.cs
+++ CSharp-Advanced/More-Exercises/Problem2-Vapor-Store/Program.cs
@@ -7,36 +7,55 @@ namespace Problem2_Vapor_Store
         public static void Main(string[] args)
         {
             decimal moneyBalance = decimal.Parse(Console.ReadLine());
-            string game = string.Empty;
-            decimal price = 0.0m;
-            decimal sumSpent = 0.0m;
-
-            for (int i = 1;; i++)
+            string game;
+            decimal moneyInStart = moneyBalance;
+            do
             {
                 game = Console.ReadLine();
-                switch (game)
+                if (game != "Game Time")
                 {
-                    case "OutFall 4":
-                        price = 39.99m;
-                        break;
-                    case "CS: OG":
-                        price = 15.99m;
-                        break;
-                    case "Zplinter Zell":
-                        price = 19.99m;
-                        break;
-                    case "Honored 2":
-                        price = 59.99m;
-                        break;
-                    case "RoverWatch":
-                        price = 29.99m;
-                        break;
-                    case "RoverWatch Origins Edition":
-                        price = 39.99m;
-                        break;
-                    default: break;
+                    decimal price;
+                    switch (game)
+                    {
+                        case "OutFall 4":
+                            price = 39.99m;
+                            break;
+                        case "CS: OG":
+                            price = 15.99m;
+                            break;
+                        case "Zplinter Zell":
+                            price = 19.99m;
+                            break;
+                        case "Honored 2":
+                            price = 59.99m;
+                            break;
+                        case "RoverWatch":
+                            price = 29.99m;
+                            break;
+                        case "RoverWatch Origins Edition":
+                            price = 39.99m;
+                            break;
+                        default:
+                            Console.WriteLine("Not Found");
+                            continue;
+                    }
+                    if (price > moneyBalance)
+                    {
+                        Console.WriteLine("Too Expensive");
+                        continue;
+                    }
+                    if (moneyBalance <= 0)
+                    {
+                        Console.WriteLine("Out of money!");
+                        return;
+                    }
+                    Console.WriteLine($"Bought {game}");
+                    moneyBalance = moneyBalance - price;
                 }
-            }
+            } while (game != "Game Time");
+            Console.WriteLine(moneyBalance <= 0
+                ? "Out of money!"
+                : $"Total spent: ${moneyInStart - moneyBalance:f2}. Remaining: ${moneyBalance:f2}");
         }
     }
 }
