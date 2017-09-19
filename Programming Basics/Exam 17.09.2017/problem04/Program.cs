ded7f1b07f2cfdd1ae3938c5eecfe9610df676f2
PlamenMI81-D <plamko@abv.bg>
2017-09-19 23:23:08 +0300

    new proj

---
+++ Programming Basics/Exam 17.09.2017/problem04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace problem04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double rakiaLiters = 0.0;
+            double rakiaDegrees = 0.0;
+            double sumRakiaLiters = 0.0;
+            double sumRakiaDegrees = 0.0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                rakiaLiters = double.Parse(Console.ReadLine());
+                rakiaDegrees = double.Parse(Console.ReadLine());
+                sumRakiaLiters += rakiaLiters;
+                sumRakiaDegrees += rakiaLiters * rakiaDegrees;
+            }
+
+            double totalDegrees = sumRakiaDegrees / sumRakiaLiters;
+            Console.WriteLine($"Liter: {sumRakiaLiters:f2}\n" +
+                              $"Degrees: {totalDegrees:f2}");
+            if (totalDegrees < 38)
+                Console.WriteLine("Not good, you should baking!");
+            else if (totalDegrees > 42)
+                Console.WriteLine("Dilution with distilled water!");
+            else
+                Console.WriteLine("Super!");
+        }
+    }
+}
---
+++ Programming Basics/Exam 17.09.2017/problem05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace problem05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = 3 * n;
+            int leftRightDots = (width - 1) / 2;
+            Console.WriteLine("{0}x{0}", new string('.', leftRightDots));
+            leftRightDots--;
+            Console.WriteLine("{0}/x\\{0}", new string('.', leftRightDots));
+            Console.WriteLine("{0}x|x{0}", new string('.', leftRightDots));
+            leftRightDots = (width - 1 - n * 2) / 2;
+            int theXSign = n;
+            for (int i = 1; i <= n / 2 + 1; i++)
+            {
+                Console.WriteLine("{0}{1}|{1}{0}", new string('.', leftRightDots), new string('x', theXSign));
+                leftRightDots--;
+                theXSign++;
+            }
+            leftRightDots += 2;
+            theXSign -= 2;
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine("{0}{1}|{1}{0}", new string('.', leftRightDots), new string('x', theXSign));
+                leftRightDots++;
+                theXSign--;
+            }
+            leftRightDots = (width - 3) / 2;
+            Console.WriteLine("{0}/x\\{0}", new string('.', leftRightDots));
+            Console.WriteLine("{0}\\x/{0}", new string('.', leftRightDots));
+            leftRightDots = (width - 1 - n * 2) / 2;
+            theXSign = n;
+            for (int i = 1; i <= n / 2 + 1; i++)
+            {
+                Console.WriteLine("{0}{1}|{1}{0}", new string('.', leftRightDots), new string('x', theXSign));
+                leftRightDots--;
+                theXSign++;
+            }
+            leftRightDots += 2;
+            theXSign -= 2;
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine("{0}{1}|{1}{0}", new string('.', leftRightDots), new string('x', theXSign));
+                leftRightDots++;
+                theXSign--;
+            }
+            leftRightDots = (width - 3) / 2;
+            Console.WriteLine("{0}x|x{0}", new string('.', leftRightDots));
+            Console.WriteLine("{0}\\x/{0}", new string('.', leftRightDots));
+            leftRightDots++;
+            Console.WriteLine("{0}x{0}", new string('.', leftRightDots));
+        }
+    }
+}
