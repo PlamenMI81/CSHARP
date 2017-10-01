c8cd53f81c38009dedf01f508154c127bb517bb1
PlamenMIvanov <plamko@abv.bg>
2017-10-02 01:32:37 +0300

    added exercises 17 to 18

---
+++ Tech Fundamentals/10-C# DATA TYPES AND VARIABLES - EXERCISES/17-Print Part of the ASCII Table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _17_Print_Part_of_the_ASCII_Table
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+            for (char i = (char) startIndex; i <= (char) endIndex; i++)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tech Fundamentals/10-C# DATA TYPES AND VARIABLES - EXERCISES/18-Different Integers Size/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _18_Different_Integers_Size
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            try
+            {
+                long numLong = long.Parse(number);
+                Console.WriteLine($"{number} can fit in:");
+            }
+            catch (Exception e)
+            {
+                
+            }
+            try
+            {
+                sbyte numSByte = sbyte.Parse(number);
+                Console.WriteLine("* sbyte");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                byte numByte = byte.Parse(number);
+                Console.WriteLine("* byte");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                short numShort = short.Parse(number);
+                Console.WriteLine("* short");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                ushort numUShort = ushort.Parse(number);
+                Console.WriteLine("* ushort");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                int numInt = int.Parse(number);
+                Console.WriteLine("* int");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                uint numUInt = uint.Parse(number);
+                Console.WriteLine("* uint");
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                long numLong = long.Parse(number);
+                Console.WriteLine("* long");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{number} can't fit in any type");
+            }
+        }
+    }
+}
