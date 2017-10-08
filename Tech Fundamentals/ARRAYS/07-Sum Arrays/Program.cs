e6e346ffd5149b3b385495b33dff640b8b387bc5
PlamenMIvanov <plamko@abv.bg>
2017-10-08 17:47:41 +0300

    upd

---
+++ Tech Fundamentals/ARRAYS/07-Sum Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Sum_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stringOne = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] stringTwo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int longerArray = Math.Max(stringOne.Length, stringTwo.Length);
+            int[] sumArrays = new int[longerArray];
+            for (int i = 0; i < longerArray; i++)
+            {
+                sumArrays[i] = stringOne[i % stringOne.Length] + stringTwo[i % stringTwo.Length];
+
+            }
+            foreach (int sumArray in sumArrays)
+            {
+                Console.Write(sumArray + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tech Fundamentals/ARRAYS/08-Condense Array to Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08_Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stringInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            while (stringInput.Length>0)
+            {
+                int[] condensed=new int[stringInput.Length-1];
+                for (int i = 0; i < stringInput.Length-1; i++)
+                {
+                    condensed[i] = stringInput[i] + stringInput[i + 1];
+                }
+
+                //How ------>  stringInput[] = condensed[]
+
+
+
+                foreach (int i in condensed)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
