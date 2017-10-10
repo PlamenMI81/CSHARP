de1dca921c8a59d6957c187284f5aae3eb31229c
PlamenMI81 <plamko@abv.bg>
2017-10-10 14:30:38 +0300

    upd

---
+++ Tech Fundamentals/14-C# ARRAYS - EXERCISES/05-Compare Char Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] first = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] second = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            int shortestArr = 0;
+            shortestArr = first.Length<=second.Length ? first.Length : second.Length;
+            string stringResult=string.Empty;
+            bool isEqual = false;
+            for (int i = 0; i < shortestArr; i++)
+            {
+                isEqual = false;
+                if (first[i]<second[i])
+                {
+                    stringResult = $"{string.Join("",first)}\r\n{string.Join("",second)}";
+                }else if (first[i] > second[i])
+                {
+                    stringResult = $"{string.Join("", second)}\r\n{string.Join("", first)}";
+                }else if (first[i]==second[i])
+                {
+                    isEqual = true;
+                }
+            }
+            if (isEqual)
+            {
+                stringResult = first.Length<=second.Length 
+                    ? $"{string.Join("", first)}\r\n{string.Join("", second)}" 
+                    : $"{string.Join("", second)}\r\n{string.Join("", first)}";
+            }
+            Console.WriteLine(stringResult);
+        }
+    }
+}
---
+++ Tech Fundamentals/14-C# ARRAYS - EXERCISES/06-Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] seq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = seq.Length - 1;
+            int element = 0;
+            int bestElement = 0;
+            int len = 1;
+            int bestLen = 1;
+            for (int i = start; i >= 1; i--)
+            {
+                if (seq[i] == seq[i - 1] )
+                {
+                    element = seq[i];
+                    len++;
+                    if (len >= bestLen)
+                    {
+                        bestElement = element;
+                        bestLen = len;
+                    }
+                }
+                else
+                {
+                    len = 1;
+                }
+            }
+            for (int i = 0; i < bestLen; i++)
+            {
+                Console.Write($"{bestElement} "); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
