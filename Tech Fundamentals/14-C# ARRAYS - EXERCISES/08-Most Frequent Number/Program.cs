e1cdc7c95331e90145963ff4f5d6dc2ed94528d5
PlamenMIvanov <plamko@abv.bg>
2017-10-11 20:28:47 +0300

    upd

--- Tech Fundamentals/14-C# ARRAYS - EXERCISES/08-Most Frequent Number/Program.cs
+++ Tech Fundamentals/14-C# ARRAYS - EXERCISES/08-Most Frequent Number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08_Most_Frequent_Number
 {
@@ -6,7 +7,33 @@ namespace _08_Most_Frequent_Number
     {
         static void Main(string[] args)
         {
-            
+            int[] sequence = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int equalSigns=0;
+            int howTimes = 1;
+            int bestSign=1;
+            int bestHowTimes = 1;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                for (int j = i + 1; j < sequence.Length; j++)
+                {
+                    if (sequence[i] == sequence[j])
+                    {
+                        equalSigns = sequence[i];
+                        howTimes++;
+                    }
+                  
+                    if (howTimes > bestHowTimes)
+                    {
+                        bestSign = equalSigns;
+                        bestHowTimes = howTimes;
+                    }
+                }
+                howTimes=1;
+            }
+            Console.WriteLine(bestSign);
         }
     }
 }
