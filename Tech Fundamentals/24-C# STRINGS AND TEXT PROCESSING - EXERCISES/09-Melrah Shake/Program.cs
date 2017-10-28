06aa08795a1c52cd43ecbf06618936c86b2fd358
PlamenMIvanov <plamko@abv.bg>
2017-10-29 01:43:16 +0300

    upd

---
+++ Tech Fundamentals/24-C# STRINGS AND TEXT PROCESSING - EXERCISES/09-Melrah Shake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09_Melrah_Shake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomString = Console.ReadLine();
+            var patern = Console.ReadLine();
+            var shakedString = "";
+            while (true)
+            {
+                var indexFirstOccurence = randomString.IndexOf(patern);
+                var indexLastOccurence = randomString.LastIndexOf(patern);
+
+                if (indexFirstOccurence > -1 && indexLastOccurence > -1 && patern.Length > 0 && indexFirstOccurence != indexLastOccurence)
+                {
+                    shakedString = randomString.Remove(indexFirstOccurence, patern.Length);
+                    shakedString = shakedString.Remove(indexLastOccurence - patern.Length, patern.Length);
+                    Console.WriteLine("Shaked it.");
+
+                    patern = patern.Remove(patern.Length / 2, 1);
+                    randomString = shakedString;
+                }
+                else
+                {
+                    Console.WriteLine("No shake.");
+                    Console.WriteLine(randomString);
+                    break;
+                }
+
+            }
+        }
+    }
+}
