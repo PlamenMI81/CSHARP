0f5e50ee0c1663dddc65957ef350c5711aa4186f
PlamenMIvanov <plamko@abv.bg>
2017-11-02 02:43:26 +0200

    upd

---
+++ Tech Fundamentals/Exam - 09 July 2017 Part 1/01-Poke Mon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01_Poke_Mon
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhautationFactor = int.Parse(Console.ReadLine());
+            var pokedCount = 0;
+            var fiftyPercentFrompower = pokePower / 2.0;
+            while (pokePower >= distanceBetweenTargets)
+            {
+                pokePower = pokePower - distanceBetweenTargets;
+                pokedCount++;
+                if (pokePower == fiftyPercentFrompower && exhautationFactor != 0)
+                {
+                    pokePower = (int)(pokePower / exhautationFactor);
+                }
+            }
+            Console.WriteLine($"{pokePower}");
+            Console.WriteLine($"{pokedCount}");
+        }
+    }
+}
---
+++ Tech Fundamentals/Exam - 09 July 2017 Part 1/02-Pokemon Don't Go/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Pokemon_Don_t_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var sumRemovedValues = 0;
+            while (input.Count > 0)
+            {
+                var returnedValue = 0;
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0)
+                {
+                    returnedValue = CopyLastIndexInFirstIndex(input);
+                }
+                else if (index > input.Count - 1)
+                {
+                    returnedValue = CopyFirstIndexInLastIndex(input, input.Count - 1);
+                }
+                else
+                {
+                    returnedValue = IncreaseDercreeseValues(input, index);
+                }
+                sumRemovedValues += returnedValue;
+            }
+            Console.WriteLine(sumRemovedValues);
+        }
+        private static int IncreaseDercreeseValues(List<int> input, int index)
+        {
+            var removedValue = input[index];
+            input.RemoveAt(index);
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] <= removedValue)
+                {
+                    input[i] += removedValue;
+                }
+                else if (input[i] > removedValue)
+                {
+                    input[i] -= removedValue;
+                }
+            }
+            return removedValue;
+        }
+
+        private static int  CopyFirstIndexInLastIndex(List<int> input, int lastIndex)
+        {
+            var lastValue = input[lastIndex];
+            input[lastIndex] = input[0];
+            var removedValue = lastValue;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] <= removedValue)
+                {
+                    input[i] += removedValue;
+                }
+                else if (input[i] > removedValue)
+                {
+                    input[i] -= removedValue;
+                }
+            }
+            return lastValue;
+        }
+
+        private static int CopyLastIndexInFirstIndex(List<int> input)
+        {
+            var firstValue = input[0];
+            input[0] = input[input.Count - 1];
+            var removedValue = firstValue;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] <= removedValue)
+                {
+                    input[i] += removedValue;
+                }
+                else if (input[i] > removedValue)
+                {
+                    input[i] -= removedValue;
+                }
+            }
+            return firstValue;
+        }
+    }
+}
