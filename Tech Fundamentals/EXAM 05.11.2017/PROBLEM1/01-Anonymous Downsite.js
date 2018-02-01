c9862b97f3a2c909466831119f58f2759b1c323e
PlamenMIvanov <plamko@abv.bg>
2018-02-01 21:05:42 +0200

    up

---
+++ Tech Fundamentals/EXAM 05.11.2017/PROBLEM1/01-Anonymous Downsite.js
@@ -0,0 +1,34 @@
+function solution(arr) {
+    let sitesDown = Number(arr[0]);
+    let securityKey = arr[1];
+    let sitesLoss = 0;
+    let countSites = 0;
+    for (let i = 2; i < sitesDown + 2; i++) {
+        let [siteName, siteVisits, siteCPPV] = arr[i].split(/\s+/g);
+        sitesLoss += Number(siteVisits) * Number(siteCPPV);
+        if (siteName !== '') {
+            countSites++;
+            console.log(siteName);
+        }
+    }
+    console.log(`Total Loss: ${sitesLoss.toFixed(20)}`);
+    let secToken = Math.pow(securityKey, countSites);
+    console.log(`Security Token: ${secToken}`);
+}
+
+solution(
+    [
+        '3',
+        '8',
+        'www.google.com 122300 94.23233',
+        'www.abv.bg 2333 11',
+        'www.kefche.com 12322 23.3222',
+        ''
+    ]
+);
+
+solution([
+    '1',
+    '1',
+    'www.facebook.com 100000 10.45'
+]);
